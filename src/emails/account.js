26c31dcd58d8e7231aae9c81a47b762a2db1d175
Umesh <umesh.khunt@gmail.com>
2021-07-01 10:31:00 -0700

    Initial commit

---
+++ src/emails/account.js
@@ -0,0 +1,50 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'umesh.khunt@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    }).then(() => {
+        console.log('Email sent')
+    }).catch((error) => {
+        console.error(error.response.body)
+    })
+}
+
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'umesh.khunt@gmail.com',
+        subject: 'Good Bye From Task App!',
+        text: `Goodbye, ${name}. Sorry to see you leave our app. I hope to see you back sometime soon.`
+    }).then(() => {
+        console.log('Email sent')
+    }).catch((error) => {
+        console.error(error.response.body)
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
+
+// const msg = {
+//     to: 'umeshp2003@gmail.com', // Change to your recipient
+//     from: 'umesh.khunt@gmail.com', // Change to your verified sender
+//     subject: 'Sending with SendGrid is Fun',
+//     text: 'and easy to do anywhere, even with Node.js',
+//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail
+//     .send(msg)
+//     .then(() => {
+//         console.log('Email sent')
+//     })
+//     .catch((error) => {
+//         console.error(error)
+//     })
