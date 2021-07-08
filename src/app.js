01f1154141e55001f0efddaec4fce621ff14b525
Umesh <umesh.khunt@gmail.com>
2021-07-07 22:01:16 -0700

    Adding test

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const   qpath = require('path')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
-
 
 
 module.exports = app
