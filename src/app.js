7e9da4317c9c43b9c3bf60dbc2108a4fde11ef99
Umesh <umesh.khunt@gmail.com>
2021-07-09 14:04:10 -0700

    Fixing corps errors for frontend

--- src/app.js
+++ src/app.js
@@ -11,6 +11,18 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
+// Middleware
+// CORPs error fix
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
